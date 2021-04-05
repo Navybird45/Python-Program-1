23edc157bc73726c0637e966568fa8319c3aaac1
Navybird45 <50809973+Navybird45@users.noreply.github.com>
2021-04-04 20:23:01 -0400

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,28 @@
+#Thomas Rose
+#Project1
+#COMS-170-WWW02-W20/21
+#Due: 01-22-2021
+#This program declares five variables, and then recalls them in five print functions.
+#All of the variables are srt- followed by a common name for that vatiable, and are strings for holding information.
+
+#Declare and initialize variables
+strName = "Thomas Rose"
+strDegree = "application developer"
+strCareerGoal = "independent application and game developer"
+strHobby = "gardening"
+strColor = "orange"
+
+#Call the string stored in each variable name as it is refrenced
+print("My name is " + strName + ".")
+print("My degree is " + strDegree + ".")
+print("My career goal is to be an " + strCareerGoal + ".")
+print("In my off time, I really enjoy " + strHobby + ".")
+print("And " + strColor + " is my favorite color.")
+
+#PROGRAM OUTPUT
+#"C:\Users\X\Desktop\Programming\Python Files\venv\Scripts\python.exe" "C:/Users/X/Desktop/Programming/Python Files/venv/Scripts/Program1.py"
+#My name is Thomas Rose.
+#My degree is application developer.
+#My career goal is to be an independent application and game developer.
+#In my off time, I really enjoy gardening.
+#And orange is my favorite color.
